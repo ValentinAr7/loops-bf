16c70cbfa2688f6fad6283ba637b9506d185c16b
Valentin <valio990210@gmail.com>
2023-04-25 19:23:05 +0300

    palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,16 +1,12 @@
 function palindrome(num) {
-
-    let palindrom = true;
-
-        let array = num.split('')
-        console.log(array);
-
-        let secondHalf = array.splice(array.length / 2)
-
-        for(let i = 0; i < secondHalf.length; i++){
-
-        }
-
-}
-
-palindrome('123321')
+    let isPalindrome = false;
+    let reversedNumber = num.toString().split('').reverse().join('');
+  
+    if (num.toString() === reversedNumber) {
+      isPalindrome = true;
+    }
+  
+    console.log(isPalindrome);
+  }
+  
+  palindrome(123321);
