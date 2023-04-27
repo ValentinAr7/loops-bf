b839fce9628d7c8df1a82a7d3b457f1ef15a54af
Valentin <valio990210@gmail.com>
2023-04-27 17:15:58 +0300

    chages

---
+++ Data Structures/task1.js
@@ -0,0 +1,19 @@
+function task1(){
+
+    let arrayOfPapers = [0, 1, 1, 0, 0, 1, 1, 1]
+    //where 0 == plain paper
+    // and  1 == glossy paper
+
+    let arrayOfTasks = [1, 1, 0, 0, 1, 0]
+    // each task requieres the type of paper based on the 1s and 0s
+
+    // for(let i = 0; i < arrayOfPapers.length; i++){
+    //     let currentPaper = arrayOfPapers[i] 
+
+
+    //     console.log(i);
+    // }
+
+}
+
+
